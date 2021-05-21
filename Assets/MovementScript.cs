ec4e1a7a6ea2aafdcc9cc37196dcda24f76bb8dc
nimshi89 <krajcsirik20@gmail.com>
2021-05-21 19:36:13 +0100

    Created a basic movement system with animations, These are just placeholders and downloaded them for the Unity asset store.

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{   
+    [Range(0, 10)]
+    [TooltipAttribute("This variable will set the speed of the movement")]
+    public float movementSpeed = 5f;
+
+
+    public Rigidbody2D rb;      //referencing the rigidbody2d that is used for the player
+    public Animator animator;
+
+    Vector2 movement;       //storing the movement input
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");       //Updating the movement on the Horizontal axis
+        movement.y = Input.GetAxisRaw("Vertical");      //Updating the movement on the Vertical axis
+        
+        animator.SetFloat("Horizontal", movement.x);        //Changing the float value in the animator based on input
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);      //Setting speed value with a squared magnitude for better optimization
+    }   
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);      //applying the movement to the rigidbody
+    }
+}
