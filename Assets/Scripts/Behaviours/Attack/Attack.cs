d31b97c82adc8004709c0c149e32f411282f9086
nimshi89 <krajcsirik20@gmail.com>
2021-06-05 16:49:13 +0100

    started making the basic combat

--- Assets/Scripts/Behaviours/Attack/Attack.cs
+++ Assets/Scripts/Behaviours/Attack/Attack.cs
@@ -12,11 +12,61 @@ public class Attack : Behaviour
     [SerializeField]
     protected GameObject oldTarget = null;
 
+    public Animator animator;
+
+    [Tooltip("The object that will be used to detect enemies")]
+    public Transform AttackArea;
+
+    [Range(0,2)]
+    [Tooltip("Variable to set the radius for the Attack Area")]
+    public float attackRange = 0.5f;
+
+    [Range(0, 25)]
+    [Tooltip("Variable to set the attack damage dealt")]
+    public float Damage = 25f;
+
+    [Tooltip("Variable to set which layers to detect enemies on")]
+    public LayerMask EnemyLayers;
+
     public void Start()
     {
     }
 
+    void Strike()       //declaring function for attacking
+    {
+
+        animator.SetTrigger("Attack");      //calls in the appropriate triger fopr the animator to play corresponding animation
+
+        Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(AttackArea.position, attackRange, EnemyLayers);
+
+        foreach (Collider2D enemy in hitEnemy)
+        {
+
+            Debug.Log("HIT" + enemy.name);
+
+            enemy.GetComponent<Health>().TakeDamage(Damage);
+        
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (AttackArea == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(AttackArea.position, attackRange);
+    }
+
     public void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Space))        //condition for input(temorary)
+        {
+
+            Strike();       //attacking
+            
+        }
     }
 }
